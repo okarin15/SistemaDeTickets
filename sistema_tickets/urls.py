4e37ee0b4408738e0263da7fb33a57eb89d7d686
okarin15 <jackds15rv@gmail.com>
2025-12-09 15:10:49 -0300

    Add files via upload

--- sistema_tickets/urls.py
+++ sistema_tickets/urls.py
@@ -15,15 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tickets.urls')),
 ]
 
-# ESTO ES LO NUEVO: Permite ver las fotos subidas en desarrollo
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        re_path(r'^static/(?P<path>.*)$', serve, {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    ]
+# ===================================================================
+
+# Configuración normal para modo DEBUG=True
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
